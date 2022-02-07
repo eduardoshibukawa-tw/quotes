524336b0d80ac96241ac6eb508778bb60ceba1e6
Eduardo Shibukawa <eduardo.shibukawa@thoughtworks.com>
2022-02-07 10:35:00 -0300

    creating project

---
+++ yoda/src/main/java/com/eduardoshibukawatw/yoda/YodaApplication.java
@@ -0,0 +1,13 @@
+package com.eduardoshibukawatw.yoda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YodaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YodaApplication.class, args);
+	}
+
+}
---
+++ yoda/src/test/java/com/eduardoshibukawatw/yoda/YodaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardoshibukawatw.yoda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YodaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
